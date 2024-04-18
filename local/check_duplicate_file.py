da07a2b9ae52546b09e258299589a1a4cdea04fc
Ian Ronk <ianronk0@gmail.com>
2024-04-18 15:10:45 +0200

    Updare download file

--- local/check_duplicate_file.py
+++ local/check_duplicate_file.py
@@ -1,13 +1,25 @@
+import pandas as pd
+from urllib.request import urlretrieve
 import os
 import requests
 
 
-def compare_size(file_path):
-    local = os.path.checksize(file_path) 
-    req = requests.get("http://49.13.173.177/file_size")
-    content = eval(req.content.decode("utf-8"))
-    remote = content['size']
-    if local == remote:
-        return True
+def download_data(file_name="data.json"):
+    retrieved = False
+    if file_name in os.listdir():
+        local = os.path.getsize(file_name)
+        req = requests.get("http://49.13.173.177/file_size")
+        content = eval(req.content.decode("utf-8"))
+        remote = content["size"]
+        if local == remote:
+            retrieved = True
+        else:
+            retrieved = False
+
+    if not retrieved:
+        print("File not found, downloading now")
+        url = "http://49.13.173.177/data/data.json"
+        urlretrieve(url, file_name)
     else:
-        return False
+        print("File already found in directory")
+    return True
